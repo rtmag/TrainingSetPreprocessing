b66a0b04d15e28a46bc7aa7e887ffd44bb10a4ec
Roberto Tirado Magallanes <rtmag4@gmail.com>
2017-02-23 12:06:59 +0800

    Update bisulfite_diagnostik_plots.R

--- bisulfite_diagnostik_plots.R
+++ bisulfite_diagnostik_plots.R
@@ -1,5 +1,11 @@
-
+# Reading in bismark cytosine report 
 data = read.table( pipe("zcat combine_hct116_WGBS_1_bismark_bt2_pe.coverage.report.gz|cut -f 4,5,6"), 
                   colClasses=c("integer", "integer", "character") )
 
-coverage = data[,1]+data[,2]
+# Coverage barplot
+coverage = data[,1] + data[,2]
+coverage[coverage>15] = 15
+barplot(table(coverage),border=NA,col="darkred")
+
+# Methylation level distribution
+
