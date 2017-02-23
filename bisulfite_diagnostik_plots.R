7c97797f0907e767f2396fec747a37c62ee91153
Roberto Tirado Magallanes <rtmag4@gmail.com>
2017-02-23 18:45:14 +0800

    Update bisulfite_diagnostik_plots.R

--- bisulfite_diagnostik_plots.R
+++ bisulfite_diagnostik_plots.R
@@ -3,16 +3,19 @@ data = read.table( pipe("zcat combine_hct116_WGBS_1_bismark_bt2_pe.coverage.repo
                   colClasses=c("integer", "integer", "character") )
 
 # open plot file
-pdf("Diagnostik_plots_HCT116_CY.pdf")
+pdf("Diagnostik_coverage_HCT116_CY.pdf")
 
 ### Coverage barplot ###
 coverage = data[,1] + data[,2]
 coverage[coverage>15] = 15
 
 barplot( table(coverage), border=NA, col="darkred", main="Coverage all Cs")
-
+dev.off()
 ### Methylation level distribution ###
 # set coverage threshold first
+
+pdf("Diagnostik_beta_HCT116_CY.pdf")
+coverage = data[,1] + data[,2]
 cov.ix = coverage > 9
 
 # plot
