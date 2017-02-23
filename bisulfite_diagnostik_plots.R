a281a139c9fca0f34dabcc988c8892996b114ff2
Roberto Tirado Magallanes <rtmag4@gmail.com>
2017-02-23 12:02:28 +0800

    Update bisulfite_diagnostik_plots.R

--- bisulfite_diagnostik_plots.R
+++ bisulfite_diagnostik_plots.R
@@ -1,3 +1,5 @@
 
 data = read.table( pipe("zcat combine_hct116_WGBS_1_bismark_bt2_pe.coverage.report.gz|cut -f 4,5,6"), 
                   colClasses=c("integer", "integer", "character") )
+
+coverage = data[,1]+data[,2]
