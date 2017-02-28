c7fa63b1af68f3a4ca72225285bb594dbaa42e28
Roberto Tirado Magallanes <rtmag4@gmail.com>
2017-02-28 11:54:16 +0800

    Update birmark_report_thresholdFilter.sh

--- birmark_report_thresholdFilter.sh
+++ birmark_report_thresholdFilter.sh
@@ -1,7 +1,7 @@
 
 zcat combine_hct116_WGBS_1_bismark_bt2_pe.coverage.report.gz | awk -F'\t' '{ $COV=($4+$5); $B=($4/$COV) ; \
                                                                           if( $COV>4 ){ if( $B>=0.8 ){ \
-                                                                                print $1"\t"$2"\t"$3"\t"M \
-                                                                          }if( $COV>4 ){ if( $B<=0.2 ){ \
-                                                                                print $1"\t"$2"\t"$3"\t"U \
-                                                                          } }' > FILTERED.txt
+                                                                                print $1"\t"$2"\t"$3"\t"M } } \
+                                                                          if( $COV>4 ){ if( $B<=0.2 ){ \
+                                                                                print $1"\t"$2"\t"$3"\t"U } } \
+                                                                           }' > FILTERED.txt
