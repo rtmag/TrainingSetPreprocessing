40e1d50c66896e7424a2db282b8fcf81655a1672
Roberto Tirado Magallanes <rtmag4@gmail.com>
2017-02-28 18:48:50 +0800

    Update 5.GraphMapCoordinates_FastaCorrection.sh

--- 5.GraphMapCoordinates_FastaCorrection.sh
+++ 5.GraphMapCoordinates_FastaCorrection.sh
@@ -1,1 +1,5 @@
 samtools view -Sb hct116_correct_pass_alignment.sam | bedtools bamtobed -i - > hct116_correct_pass_alignment.bed
+
+more hct116_correct_pass_alignment.bed|grep "9102bb6e-5363-4fee-9ac7-05af8b497733_Basecall_2D_2d"| \
+bedtools getfasta -fi HCT116_MUHV.fasta -bed - > hct116_ch9_read990_metri_METHYL.fasta
+
