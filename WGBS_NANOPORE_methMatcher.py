4cd4bb08f8cbbc2e7ed34b0cb8b7e7b7c883bc04
Roberto Tirado Magallanes <rtmag4@gmail.com>
2017-05-25 13:45:12 +0800

    Update WGBS_NANOPORE_methMatcher.py

--- WGBS_NANOPORE_methMatcher.py
+++ WGBS_NANOPORE_methMatcher.py
@@ -9,11 +9,20 @@ import gzip
 WGBS_file = argv[1] 
 NANO_file = argv[2]
 
-
 WGBS = {}
 with gzip.open(WGBS_file, "r") as file:
     for line in file:
         line=line.split("\t")
         WGBS[ ( str(line[0]), int(line[1]) ) ] = float(line[3])
     
+file.close()
 
+with open(NANO_file, "r") as file:
+    for line in file:
+        line = line.split("\t")
+        WGBS_meth = WGBS.get( ( str(line[0]), int(line[1]) ) ) or False
+        WGBS_meth = str(WGBS_meth)
+        if WGBS_meth:
+            print '{}\t{}\t{}\t{}'.format(str(line[0]),int(line[1]),float(WGBS_meth),float(line[3]))
+      
+file.close()
