16f5813faf6e946f6422cf9f2ae6de66f9b09b69
Roberto Tirado Magallanes <rtmag4@gmail.com>
2017-05-25 13:19:25 +0800

    Update WGBS_NANOPORE_methMatcher.py

--- WGBS_NANOPORE_methMatcher.py
+++ WGBS_NANOPORE_methMatcher.py
@@ -4,10 +4,16 @@
 
 from sys import argv
 import re
-
+import gzip
   
 WGBS_file = argv[1] 
 NANO_file = argv[2]
 
 
 WGBS = {}
+with gzip.open(WGBS_file, "r") as file:
+    for line in file:
+        line=line.split("\t")
+        WGBS[ ( str(line[0]), int(line[1]) ) ] = float(line[3])
+    
+
