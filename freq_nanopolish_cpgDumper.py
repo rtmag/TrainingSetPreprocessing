e9ab2882b39cab4529ca4f09285a9404fcb9f352
Roberto Tirado Magallanes <rtmag4@gmail.com>
2017-05-25 12:03:31 +0800

    Update freq_nanopolish_cpgDumper.py

--- freq_nanopolish_cpgDumper.py
+++ freq_nanopolish_cpgDumper.py
@@ -1,26 +1,45 @@
 from sys import argv
 import re
 
-filename = argv[1]
+if argv<3:
+    print "usage: python script.py mode[forward,reverse] inputFile"
+    break
+    
+mode = argv[1] 
+filename = argv[2]
 
-
-with open(filename, "r") as file:
-    for line in file:
-        line=line.split("\t")
+if mode == "forward":
+    with open(filename, "r") as file:
+        for line in file:
+            line=line.split("\t")
         
-        if line[3]>2:
-            index=[m.start() for m in re.finditer('CG', line[7])]
-            first=True
-            for ix in index:
-                if not first:
-                    nb=int(ix)-int(index[0])
-                    print '{}\t{}\t{}\t{}'.format(line[0],int(line[1])+nb,int(line[1])+nb,float(line[6])*100)
-                if first:
-                    print '{}\t{}\t{}\t{}'.format(line[0],line[1],int(line[1]),float(line[6])*100) 
-                    first=False 
-        if line[3]==2:
-            print '{}\t{}\t{}\t{}'.format(line[0],line[1],int(line[1])+1,float(line[6])*100) 
-            print '{}\t{}\t{}\t{}'.format(line[0],int(line[2])+1,int(line[2])+1,float(line[6])*100) 
+            if int(line[3])>1:
+                index=[m.start() for m in re.finditer('CG', line[7])]
+                first=True
+                for ix in index:
+                    if not first:
+                        nb=int(ix)-int(index[0])
+                        print '{}\t{}\t{}\t{}'.format(line[0],int(line[1])+nb,int(line[1])+nb,float(line[6])*100)
+                    if first:
+                        print '{}\t{}\t{}\t{}'.format(line[0],line[1],int(line[1]),float(line[6])*100) 
+                        first=False 
+            if int(line[3])==1:
+                print '{}\t{}\t{}\t{}'.format(line[0],line[1],int(line[1]),float(line[6])*100) 
 
-        if line[3]==1:
-            print '{}\t{}\t{}\t{}'.format(line[0],line[1],int(line[1]),float(line[6])*100) 
+if mode == "reverse":
+    with open(filename, "r") as file:
+        for line in file:
+            line=line.split("\t")
+        
+            if int(line[3])>1:
+                index=[m.start() for m in re.finditer('CG', line[7])]
+                first=True
+                for ix in index:
+                    if not first:
+                        nb=int(ix)-int(index[0])
+                        print '{}\t{}\t{}\t{}'.format(line[0],int(line[1])+nb+1,int(line[1])+nb+1,float(line[6])*100)
+                    if first:
+                        print '{}\t{}\t{}\t{}'.format(line[0],int(line[1])+1,int(line[1])+1,float(line[6])*100) 
+                        first=False 
+            if int(line[3])==1:
+                print '{}\t{}\t{}\t{}'.format(line[0],int(line[1])+1,int(line[1])+1,float(line[6])*100) 
