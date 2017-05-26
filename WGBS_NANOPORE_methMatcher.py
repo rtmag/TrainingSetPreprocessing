33e2b2a43b7ad3c80a078177379f77c38b40689f
Roberto Tirado Magallanes <rtmag4@gmail.com>
2017-05-26 16:21:19 +0800

    Update WGBS_NANOPORE_methMatcher.py

--- WGBS_NANOPORE_methMatcher.py
+++ WGBS_NANOPORE_methMatcher.py
@@ -13,18 +13,23 @@ WGBS = {}
 with gzip.open(WGBS_file, "r") as file:
     for line in file:
         line=line.split("\t")
-        if (line[3]+line[4])>0:
-          if str(line[5])=="CG"
-             WGBS[ ( str(line[0]), int(line[1]) ) ] = float(line[3](line[3]+line[4]))
+        if (int(line[3])+int(line[4]))>0:
+          if str(line[5])=="CG":
+             WGBS[ ( str(line[0]), int(line[1]) ) ] = float(line[3])/(float(line[3])+float(line[4])) 
     
 file.close()
 
+NANO_file = 'hct116_pass_template_nanopolish_bwa_hct116corrected_reverse_methylation_freq_CGdump.tsv'
+output = open('WGBS_NANOreverse_2.txt', 'w')
+
 with open(NANO_file, "r") as file:
     for line in file:
+        line = line.rstrip()
         line = line.split("\t")
-        WGBS_meth = WGBS.get( ( str(line[0]), int(line[1]) ) ) or False
-        WGBS_meth = str(WGBS_meth)
-        if WGBS_meth:
-            print '{}\t{}\t{}\t{}'.format(str(line[0]),int(line[1]),float(WGBS_meth),float(line[3]))
+        WGBS_meth = WGBS.get( ( str(line[0]), int(line[1]) ) ) or "EMPTY"
+        if WGBS_meth != 'EMPTY':
+            output.write('{}\t{}\t{}\t{}\n'.format(str(line[0]),int(line[1]),float(WGBS_meth)*100,float(line[3]) ) )
       
+output.close()
 file.close()
+
