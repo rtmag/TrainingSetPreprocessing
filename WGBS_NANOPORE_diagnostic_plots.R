edbdc29d960fb7113cbb2e4986c3efc07f6f135d
Roberto Tirado Magallanes <rtmag4@gmail.com>
2017-05-26 18:19:39 +0800

    Update WGBS_NANOPORE_diagnostic_plots.R

--- WGBS_NANOPORE_diagnostic_plots.R
+++ WGBS_NANOPORE_diagnostic_plots.R
@@ -6,11 +6,11 @@ par(mfrow=c(3,2))
 fwd=read.table("WGBS_NANOforward_2.txt",sep="\t")
 rev=read.table("WGBS_NANOreverse_2.txt",sep="\t")
 
-smoothScatter(fwd[,3],fwd[,4],main="Forward strand",xlab="WGBS M-Levels",ylab="Nanopore M-level")
-smoothScatter(rev[,3],rev[,4],main="Forward reverse",xlab="WGBS M-Levels",ylab="Nanopore M-level")
+smoothScatter(fwd[,3],fwd[,4],main="Forward strand",xlab="WGBS M-Levels",ylab="Nanopore Methylation level")
+smoothScatter(rev[,3],rev[,4],main="Reverse strand",xlab="WGBS M-Levels",ylab="Nanopore Methylation level")
 
 plot(density(fwd[,3]),main="Forward strand WGBS")
-plot(density(fwd[,4]),main="Forward strand Nanopore")
-
 plot(density(rev[,3]),main="Reverse strand WGBS")
+
+plot(density(fwd[,4]),main="Forward strand Nanopore")
 plot(density(rev[,4]),main="Reverse strand Nanopore")
